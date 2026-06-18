13f050f94331faced800ffbc4689dc59400923ce
jmf3658 <jfullmer@austin.utexas.edu>
2026-06-18 15:18:56 -0500

    Resolve merge conflict

--- plugin-updater.php
+++ plugin-updater.php
@@ -94,11 +94,7 @@ function utexas_smtp_helper_get_remote_data() {
 
 function utexas_smtp_helper_move_plugin( $result, $options ) {
 	if ( ! array_key_exists( 'plugin', $options ) ) {
-<<<<<<< HEAD
-		return;
-=======
 		return $result;
->>>>>>> develop
 	}
 	// Only move if the original plugin was in the expected location.
 	if ( UtexasSmtpPluginUpdater::$slug . '/' . UtexasSmtpPluginUpdater::$slug . '.php' !== $options['plugin'] ) {
